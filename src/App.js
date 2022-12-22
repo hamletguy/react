1e06350eabbbbe7d9bcec65290446f9cdff7d284
hamletguy <ahmedayachi1607@gmail.com>
2022-12-22 09:40:53 +0100

    fixed html to jsx

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 function App() {
   return (
-    <div class="form">
-      <div class="title">Welcome</div>
-      <div class="subtitle">Let's create your account!</div>
-      <div class="input-container ic1">
-        <input id="firstname" class="input" type="text" placeholder=" " />
-        <div class="cut"></div>
-        <label for="firstname" class="placeholder">
+    <div className="form">
+      <div className="title">Welcome</div>
+      <div className="subtitle">Let's create your account!</div>
+      <div className="input-container ic1">
+        <input id="firstname" className="input" type="text" placeholder=" " />
+        <div className="cut" />
+        <label htmlFor="firstname" className="placeholder">
           First name
         </label>
       </div>
-      <div class="input-container ic2">
-        <input id="lastname" class="input" type="text" placeholder=" " />
-        <div class="cut"></div>
-        <label for="lastname" class="placeholder">
+      <div className="input-container ic2">
+        <input id="lastname" className="input" type="text" placeholder=" " />
+        <div className="cut" />
+        <label htmlFor="lastname" className="placeholder">
           Last name
         </label>
       </div>
-      <div class="input-container ic2">
-        <input id="email" class="input" type="text" placeholder=" " />
-        <div class="cut cut-short"></div>
-        <label for="email" class="placeholder">
+      <div className="input-container ic2">
+        <input id="email" className="input" type="text" placeholder=" " />
+        <div className="cut cut-short" />
+        <label htmlFor="email" className="placeholder">
           Email
         </label>
       </div>
-      <button type="text" class="submit">
+      <button type="text" className="submit">
         submit
       </button>
     </div>
